7d97b9af01538149a8c6ffb3956441c99d0f9753
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-12-17 10:56:48 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CurateVanilla::Application.config.secret_key_base = '385b13466c017df4012d6365078893d14f9fdc5c1a06e0255897758e4626b0cf622c2f1337f4018271492dfba4102af8ad21f4d3e01a28a4f049927ddbfa748d'
