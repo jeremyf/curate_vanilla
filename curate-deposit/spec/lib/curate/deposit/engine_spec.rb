0cac7ec03b57f1865f75aa860c56c00edf1ec64a
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-01-16 09:46:00 -0500

    Adding spec for helpful class name

--- curate-deposit/spec/lib/curate/deposit/engine_spec.rb
+++ curate-deposit/spec/lib/curate/deposit/engine_spec.rb
@@ -3,9 +3,12 @@ require 'spec_helper'
 module Curate::Deposit
   describe Engine do
     context '.register_new_form_for' do
+      let(:helpful_class_name) { /\ACurate::FormClassBuilder/ }
       context ':work form' do
         subject { Curate::Deposit.finalize_new_form_for(:work) }
         its(:work_type) { should eq :work }
+        its(:inspect) { should match helpful_class_name }
+        its(:to_s) { should match helpful_class_name }
         its(:finalizer_config) { should be_a_kind_of Hash }
         it { should respond_to(:new) }
       end
@@ -13,6 +16,8 @@ module Curate::Deposit
       context ':article form' do
         subject { Curate::Deposit.finalize_new_form_for(:article) }
         its(:work_type) { should eq :article }
+        its(:inspect) { should match helpful_class_name }
+        its(:to_s) { should match helpful_class_name }
         its(:finalizer_config) { should be_a_kind_of Hash }
         it { should respond_to(:new) }
       end
